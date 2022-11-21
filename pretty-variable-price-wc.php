342059f27d18d300ee77759fb991703fee419fd1
Vladimir Udachin <udachin@motiw.ru>
2022-11-21 21:43:04 +0300

    add simple product data

--- pretty-variable-price-wc.php
+++ pretty-variable-price-wc.php
@@ -27,34 +27,58 @@ function show_pretty_price( $atts ){
 
     ), $atts );
     global $product;
-    $product_variations = $product->get_available_variations();
-    $product_prices = array();
-    $product_attrs = array();
-    $price_str = '';
-    $attrs_str = '';
-    foreach ($product_variations as $variation){
-        if(!empty($variation['display_price'])) array_push($product_prices, $variation['display_price']);
-        if(!empty($variation['attributes'])){
-            reset($variation['attributes']);
-            array_push($product_attrs, current($variation['attributes']));
+    $calcBody = '';
+    if(!empty($product) && is_object($product)){
+        $product_prices = array();
+        $product_attrs = array();
+        $price_str = '';
+        $attrs_str = '';
+        switch (get_class($product)){
+            case 'WC_Product_Variable':
+                $product_variations = $product->get_available_variations();
+                foreach ($product_variations as $variation){
+                    if(!empty($variation['display_price'])) array_push($product_prices, $variation['display_price']);
+                    if(!empty($variation['attributes'])){
+                        reset($variation['attributes']);
+                        array_push($product_attrs, current($variation['attributes']));
+                    }
+                }
+                break;
+            case 'WC_Product_Simple':
+                $product_price = $product->get_price();
+                $product_attrib = $product->get_attributes();
+                if(!empty($product_price)) array_push($product_prices, $product_price);
+                if(!empty($product_attrib)){
+                    reset($product_attrib);
+                    $attrib = current($product_attrib);
+                    if(is_object($attrib) && get_class($attrib) == 'WC_Product_Attribute'){
+                        $attrib_data = $attrib->get_data();
+                        if(!empty($attrib_data) && !empty($attrib_data['value'])) array_push($product_attrs, $attrib_data['value']);
+                    }
+                }
+                break;
         }
-    }
-    if(!empty($product_prices) && $atts['show_prices'] === '1'){
-        $price_str = implode($atts['delimiter'] , $product_prices) . $atts['currency_symbol'];
-    }
-    if(!empty($product_attrs) && $atts['show_attrs'] === '1'){
-        $attrs_str = implode($atts['delimiter'] , $product_attrs) . $atts['after_attr'];
-    }
-    $calcBody = '
-    <div class="pretty_price_container">
-        <div class="pretty_price_row">
-        '. $price_str .'
+
+
+        if(!empty($product_prices) && $atts['show_prices'] === '1'){
+            $price_str = implode($atts['delimiter'] , $product_prices) . $atts['currency_symbol'];
+        }
+        if(!empty($product_attrs) && $atts['show_attrs'] === '1'){
+            $attrs_str = implode($atts['delimiter'] , $product_attrs) . $atts['after_attr'];
+        }
+        $calcBody = '
+        <div class="pretty_price_container">
+            <div class="pretty_price_row prices">
+            
+            '. $price_str .'
+            </div>
+            <div class="pretty_price_row attrs">
+            '. $attrs_str .'
+            </div>        
         </div>
-        <div class="pretty_price_row">
-        '. $attrs_str .'
-        </div>        
-    </div>
-    ';
+        ';
+    }
+
     return $calcBody;
 
 }
